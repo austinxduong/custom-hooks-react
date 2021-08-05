dc634b350ef012a00526bc866de7327d4b142f85
austinxduong <axduong2@gmail.com>
2021-08-05 12:04:15 -0700

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.css';
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <h1> All Characters </h1>
+      <Link to={'/'}> Home </Link>
+    </header>
+  );
+};
+
+export default Header;
