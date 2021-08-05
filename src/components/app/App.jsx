90ec5c657b97981b6a992092997755ab0a888dac
austinxduong <axduong2@gmail.com>
2021-08-05 12:10:56 -0700

    caught a bug

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Swtich, Route } from 'react-router-dom';
-
+import { Switch, Route } from 'react-router-dom';
+import CharacterDetails from '../details/CharacterDetails';
+import AvatarList from '../characters/CharacterList';
 
 // export default function App() {
 //   return <h1>Hello World</h1>;
 // }
+
+export default function App() {
+  return (
+    <Switch>
+      <Route exact path="/"
+        component={AvatarList} />
+
+      <Route exact path="/:_id"
+        component={CharacterDetails} />
+    </Switch>
+  );
+}
 
